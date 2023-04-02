df61717ccd00f4b8778253b4cb720cdfd4e1d414
Sakib <kazisakib556@gmail.com>
2023-04-02 12:29:20 +0600

    flutter_bluetooth_serial used, only android

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:ebike_app/views/dashboard_sceen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Lidar',
+      theme: ThemeData(),
+      builder: EasyLoading.init(),
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Lidar"),
+          ),
+          body: const DashBoardScreen()),
+    );
+  }
+}
